a17021e486266a0a016571db5a4a27bcd8998f58
jennslau <jenny.lau@bitwave.io>
2025-07-10 20:23:04 -0700

    Delete src/store directory

--- src/store/tasks.ts
+++
@@ -1,6 +0,0 @@
-import { defineStore } from 'pinia';
-import { ref, computed } from 'vue';
-import type { Task } from '@/types';
-import { sampleInternalTasks, phaseProgress } from '@/data/internalTasks';
-
-export const useTaskStore = defineStore('
